c0b9969889c384530a2d039cf2af0771fded21a2
MidgardSerpent <l.voss@campus.tu-berlin.de>
2024-04-28 20:18:51 +0200

    Query 000 wurde angepasst mit dem Versuch Statisiken zu löschen, queries auszuführen und danach wieder statistiken anzustellen .. muss geprüft werden von Stefan

--- Queries/Test_Queries/000.sql
+++ Queries/Test_Queries/000.sql
@@ -1,6 +1,44 @@
+-- Zuerst Statistiken zurücksetzen mit vaccuum full -- dauert etwas
+VACUUM FULL ANALYZE;
 
-EXPLAIN --(ANALYZE, BUFFERS, FORMAT TEXT)
 
+ALTER SYSTEM SET autovacuum = off; -- autovacuum ausschalten
+ALTER SYSTEM SET autovacuum_analyze_threshold = 1000000;
+ALTER SYSTEM SET autovacuum_analyze_scale_factor = 0;
+
+SELECT pg_reload_conf(); -- umsetzung bestätigen lassen
+
+EXPLAIN ANALYSE     -- ausführung einer query mit ausführungszeit
+SELECT MIN(mc.note) AS production_note,
+       MIN(t.title) AS movie_title,
+       MIN(t.production_year) AS movie_year
+FROM company_type AS ct,
+     info_type AS it,
+     movie_companies AS mc,
+     movie_info_idx AS mi_idx,
+     title AS t
+WHERE ct.kind = 'production companies'
+  AND it.info = 'top 250 rank'
+  AND mc.note NOT LIKE '%(as Metro-Goldwyn-Mayer Pictures)%'
+  AND (mc.note LIKE '%(co-production)%'
+       OR mc.note LIKE '%(presents)%')
+  AND ct.id = mc.company_type_id
+  AND t.id = mc.movie_id
+  AND t.id = mi_idx.movie_id
+  AND mc.movie_id = mi_idx.movie_id
+  AND it.id = mi_idx.info_type_id;
+
+
+
+ALTER SYSTEM SET autovacuum = on; -- Statistiken wieder an schalten
+ALTER SYSTEM SET autovacuum_analyze_threshold = 50;
+ALTER SYSTEM SET autovacuum_analyze_scale_factor = 0.1;
+
+-- Änderungen laden
+SELECT pg_reload_conf();
+
+
+EXPLAIN ANALYSE
 SELECT MIN(mc.note) AS production_note,
        MIN(t.title) AS movie_title,
        MIN(t.production_year) AS movie_year
